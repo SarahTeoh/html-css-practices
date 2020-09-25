ab35ba63af7869d00934367ef8a578b34aa644be
SarahTeoh <you@example.com>
2020-09-25 13:16:40 +0900

    Add game logic

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -2,5 +2,25 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// Generate the target value
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+// Determine if the human or computer wins
+const compareGuesses = (currentHumanGuess, computerGuess, target) => {
+    let humanDifference = Math.abs(target - currentHumanGuess);
+    let computerDifference = Math.abs(target - computerGuess);
+    
+    if (humanDifference > computerDifference) {
+        return 'false';
+    } else {
+        return 'true';
+    }
+};
+
+// Update the correct score
+const updateScore = winner => winner === 'human' ? humanScore += 1 : computerScore += 1;
+
+// Increase the round number
+const advanceRound = () => currentRoundNumber +=1;
+
 
